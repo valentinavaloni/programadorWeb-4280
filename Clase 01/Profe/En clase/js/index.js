0803906a5d6e6f3c7ee91f5ca04e400b65dc9d0c
adrianc4 <adrian_c4@hotmail.com>
2019-04-09 20:33:05 -0300

    Add clase 01

--- Clase 01/Profe/En clase/js/index.js
+++ Clase 01/Profe/En clase/js/index.js
@@ -26,6 +26,84 @@ kgkhgjhg
 
 // Comentario
 
-var ageOfTheUser = prompt('¿Cual es tu edad?', '25')
+// var ageOfTheUser = prompt('¿Cual es tu edad?', '25')
 
-console.log(ageOfTheUser)
+// console.log('Tu edad ' + ageOfTheUser)
+
+// Mini ejercicio
+
+// var name = prompt('Cuál es tu nombre?')
+
+// var message = 'Hola ' + name
+
+// console.log(message)
+
+// Tipos de valores
+
+// String
+
+// 'Hola'
+
+// ' '
+
+// ''
+
+// 'Hola'
+
+// Undefined
+
+// var newVar
+
+// console.log(newVar)
+
+// null
+
+// var noValue = 'Hola'
+
+// noValue = null
+
+// console.log(noValue)
+
+// Number
+
+// var number = -1231
+
+// var number1 = 0
+
+// var number2 = 3.1416
+
+// Operaciones
+
+// var dolarToday = 43.4
+
+// var dolarOnWallet = prompt('Cuanto tenés hoy')
+
+// var pesosOnWallet = dolarToday * dolarOnWallet
+
+// console.log(pesosOnWallet)
+
+// var product1 = prompt('Ingresá precio leche')
+
+// var product2 = prompt('Ingresá precio Zucaritas')
+
+// var parsedProduct1 = parseFloat(product1, 10)
+
+// var parsedProduct2 = parseFloat(product2, 10)
+
+// var total = parsedProduct1 + parsedProduct2
+
+// console.log(total)
+
+// var division = 18000 / 0
+
+// console.log(division)
+
+// console.log(2 - 5)
+
+// Boleano
+
+var booleanValue1 = true
+
+var booleanValue2 = false
+
+console.log(booleanValue1, booleanValue2)
