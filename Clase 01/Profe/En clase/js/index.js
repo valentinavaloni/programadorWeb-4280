a698a9112c6f120ce963f4e0012017419103a6f3
adrianc4 <adrian_c4@hotmail.com>
2019-04-09 19:48:01 -0300

    Clase 02

---
+++ Clase 01/Profe/En clase/js/index.js
@@ -0,0 +1,31 @@
+// var $name
+
+// var _name
+
+// var name1
+
+// var firstName
+
+// var numberOfRows
+
+// var checkImport
+
+// checkImport = 'Me importe bien!'
+
+// console.log(checkImport)
+
+// checkImport = 'Soy un nuevo mensaje'
+
+// console.log(checkImport)
+
+/* 
+
+hgkhgjhg
+kgkhgjhg
+*/
+
+// Comentario
+
+var ageOfTheUser = prompt('¿Cual es tu edad?', '25')
+
+console.log(ageOfTheUser)
