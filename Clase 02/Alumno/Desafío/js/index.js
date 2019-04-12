292f59172c25a741427eba5815daae087fd4f79e
valentinavaloni <valevaloni_95@hotmail.com>
2019-04-12 15:12:33 -0300

    desafio

---
+++ Clase 02/Alumno/Desafío/js/index.js
@@ -0,0 +1,25 @@
+var message
+
+var genero = prompt('Ingresé su genero')
+
+var edad = prompt('Ingresé su edad')
+
+if (genero === 'Sr.') {
+  message = genero
+} else {
+  if (genero === 'Sra.') {
+    message = genero
+  } else {
+    if (genero === 'Sx')
+  }
+}
+
+if (edad >= 18) {
+  message = message + 'usted es mayor de edad puede ingresar'
+} else {
+  if (genero < 18) {
+    message = message + 'usted es menor de edad no puede ingresar'
+  }
+}
+console.log(message)
+
