519ce88b1c13de3d9ca80eb568a55232fdfdb676
adrianc4 <adrian_c4@hotmail.com>
2019-05-07 19:27:11 -0300

    Add clase 08

---
+++ Clase 08/Profe/En clase/js/index.js
@@ -0,0 +1,28 @@
+console.log('Es mi idola! By Undi')
+
+// var mainTitle = document.getElementById('main-title')
+
+// console.log(mainTitle)
+
+// var firstName = prompt('Ingresá tu nombre')
+
+// console.log(mainTitle.innerHTML)
+
+// var mainParagraph = document.getElementById('main-paragraph')
+
+// mainParagraph.innerHTML = prompt('Ingresá tu texto')
+
+// var user = {
+//   firstName: 'Juan',
+//   age: 30
+// }
+
+// user.firstName = 'Pedro'
+
+// console.log(user)
+
+var mainDiv = document.getElementById('main-div')
+
+console.log(mainDiv)
+
+mainDiv.innerHTML = '<h2>Pato</h2>'
