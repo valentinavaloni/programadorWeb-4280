d30578c8218dad9dfe853269eb6c84845f75a2f3
valentinavaloni <valevaloni_95@hotmail.com>
2019-05-07 19:41:01 -0300

    mini ejer

---
+++ Clase 08/Alumno/Mini ejercicio/js/index.js
@@ -0,0 +1,12 @@
+var title = document.getElementById('title')
+
+console.log(title)
+
+title.innerHTML = prompt('Ingresá un titulo')
+
+var text = document.getElementById('text')
+
+console.log(text)
+
+text.innerHTML = prompt('Ingresá un text')
+
