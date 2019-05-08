d46e40018f60e7e9124eaea6c584d3ed5a0408e2
adrianc4 <adrian_c4@hotmail.com>
2019-05-07 21:13:14 -0300

    Color example

--- Clase 08/Profe/En clase/js/index.js
+++ Clase 08/Profe/En clase/js/index.js
@@ -21,8 +21,61 @@ console.log('Es mi idola! By Undi')
 
 // console.log(user)
 
-var mainDiv = document.getElementById('main-div')
+// var mainDiv = document.getElementById('main-div')
 
-console.log(mainDiv)
+// setTimeout(function () {
+//   console.log(mainDiv)
+// }, 3000)
 
-mainDiv.innerHTML = '<h2>Pato</h2>'
+// mainDiv.innerHTML = '<h2>Pato</h2>'
+
+// var oldTitle = document.getElementById('old-title')
+
+// var newTitle = prompt('Ingresá un nuevo título')
+
+// oldTitle.innerHTML = newTitle
+
+// var orsonFarm = document.getElementsByClassName('orson-farm')
+
+// console.log(orsonFarm)
+
+// for (var i = 0; i < orsonFarm.length; i++) {
+//   var animal = orsonFarm[i]
+
+//   animal.id = i
+
+//   animal.innerHTML = 'Roy'
+// }
+
+// Busca el nodo con id farm-list
+var farmList = document.getElementById('farm-list')
+
+var animals = [
+  { name: 'Orson', color: 'pink' },
+  { name: 'Roy', color: 'red' },
+  { name: 'Wade', color: 'yellow' }
+]
+
+for (var i = 0; i < animals.length; i++) {
+  var animal = animals[i]
+
+  // Crea un nodo li
+  var li = document.createElement('li')
+
+  // Pisa el contenido del nodo li recién creado
+  li.innerHTML = '<h6>' + animal.name + '</h6>' + '<p>' + animal.color + '</p>'
+  li.style.color = animal.color
+  li.id = 'farm-list-' + i
+
+  // Agrega al final de la lista
+  farmList.appendChild(li)
+}
+
+// var index = prompt('Ingrese el elento a remover')
+
+// // Busco el elemento en el DOM, haciendo una traducción de lo que ingresó
+// // el usuario a un indice de JavaScript
+// var elementToRemove = document.getElementById('farm-list-' + (index - 1))
+
+// // Le pido al padre que lo remueva
+// elementToRemove.parentNode.removeChild(elementToRemove)
