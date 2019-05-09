29b93df039e8c6973c0d6d6579435c3c8d5e4537
adrianc4 <adrian_c4@hotmail.com>
2019-05-09 19:47:12 -0300

    Add clase 08

--- Clase 08/Profe/Desafío/js/index.js
+++ Clase 08/Profe/Desafío/js/index.js
@@ -2,7 +2,7 @@
 
 var student = {
   firstName: 'Juan',
-
+  lastName: 'Perez',
   dni: 22999333,
   email: 'juan@gmail.com'
 }
@@ -23,10 +23,20 @@ function createStudentNode (newStudent) {
   // Le setteo la clase al nodo
   liNode.className = 'list-group-item'
 
+  var fullName = ''
+
+  if (newStudent.firstName && newStudent.lastName) {
+    fullName = newStudent.firstName + ', ' + newStudent.lastName
+  } else if (newStudent.firstName) {
+    fullName = newStudent.firstName
+  } else if (newStudent.lastName) {
+    fullName = newStudent.lastName
+  }
+
   // Le agrego el contenido al nodo
   liNode.innerHTML =
     '<h1>' +
-    newStudent.getFullName() +
+    fullName +
     '</h1><h3>DNI:' +
     newStudent.dni +
     '</h3><p>E-mail:' +
---
+++ Clase 09/Temario.md
@@ -0,0 +1,7 @@
+# 9 Eventos [Modulo B]
+	
+- 9.1	¿Que son los eventos y para qué sirven?
+- 9.2	Eventos como atributos
+- 9.3	Eventos semánticos
+- 9.4	Evento click
+- 9.5	Evento blur
