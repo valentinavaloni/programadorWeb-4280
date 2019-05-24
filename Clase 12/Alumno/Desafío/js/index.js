7c61ecd32cc875c192178df5f936d6084a3fabdd
valentinavaloni <valevaloni_95@hotmail.com>
2019-05-23 21:07:03 -0300

    clase

--- Clase 12/Alumno/Desafío/js/index.js
+++ Clase 12/Alumno/Desafío/js/index.js
@@ -1,10 +1,15 @@
+var emailInput = $('#email')
+
+emailInput.blur(validateEmailField)
+
 function validateEmail (event) {
   var inputNode = $(this)
 
   var value = inputNode.val()
 
   if (!value || value.indexOf('@') === -1 || value.indexOf('.') === -1) {
-    inputNode.removeClass('is-valid').addClass('is-invalid')
+    inputNode.removeClass('is-valid')
+    inputNode.addClass('is-invalid')
   } else {
     inputNode.removeClass('is-invalid')
     inputNode.addClass('is-valid')
